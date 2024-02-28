adb245ed5609d0638b23fb1de949f52667c86e41
Samuel Wayoro <samuelwayoro@gmail.com>
2024-02-28 19:17:43 +0100

    integration jwt dans spring security :ajout du payload JWTAuthResponse ajout des proprietes authenticationEntryPoint et authenticationFilter dans la classe SecurityConfig

--- src/main/java/org/samydevup/blogrestapi/config/SecurityConfig.java
+++ src/main/java/org/samydevup/blogrestapi/config/SecurityConfig.java
@@ -1,39 +1,46 @@
 package org.samydevup.blogrestapi.config;
 
-import org.samydevup.blogrestapi.security.CustomUserDetailsService;
+import org.samydevup.blogrestapi.security.JwtAuthenticationEntryPoint;
+import org.samydevup.blogrestapi.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * * * CETTE CLASSE SERT A CONFIGURER L'AUTHENTIFICATION EN MEMOIRE OU EN BASE DE DONNEES DE SPRING SECURITY
  *
  * @Configuration : Annotation au niveau de la classe indiquant
  * qu'il s'agit d'une classe de définitions de bean(à partir de méthode annotée @Bean).
- *
  * @EnableMethodSecurity : Annotation au niveau de la classe indiquant l'activation des annotation SpringSecurity :
  * @PreAuthorize , @PostAuthorize , @Prefilter , @PostFilter
- *
  */
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
     private UserDetailsService userDetailsService;
-    public SecurityConfig(UserDetailsService userDetailsService){
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+    private JwtAuthenticationFilter authenticationFilter;
+
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          JwtAuthenticationEntryPoint authenticationEntryPoint,
+                          JwtAuthenticationFilter authenticationFilter) {
         this.userDetailsService = userDetailsService;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.authenticationFilter = authenticationFilter;
+
     }
+
     @Bean
     public static PasswordEncoder passwordEncoder() { //methode permettant l'encodage des password de user
         return new BCryptPasswordEncoder();
@@ -53,6 +60,14 @@ public class SecurityConfig {
         /**
          * Activation de la basic authentification
          * sur les endPoints de nos controllers
+
+         http.csrf((csrf) -> csrf.disable())//securise contre les attaques de type csrf
+         .authorizeHttpRequests((authorize) ->
+         //authorize.anyRequest().authenticated()) //-->commenté car demande une authentification user sur ts les endpoints
+         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()//permet un accès total sur toutes les méthodes de type GET émises sur l'url commençant par /api/
+         .requestMatchers("/api/auth/**").permitAll()
+         .anyRequest().authenticated()//demande une authentification sur toutes les autres url des endpoints
+         ).httpBasic(Customizer.withDefaults());
          */
         http.csrf((csrf) -> csrf.disable())//securise contre les attaques de type csrf
                 .authorizeHttpRequests((authorize) ->
@@ -60,7 +75,14 @@ public class SecurityConfig {
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()//permet un accès total sur toutes les méthodes de type GET émises sur l'url commençant par /api/
                                 .requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()//demande une authentification sur toutes les autres url des endpoints
-                ).httpBasic(Customizer.withDefaults());
+                ).exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        //rajouter le filtre d'authentification de jwt avant le filtre de springSecurity UsernamePasswordAuthenticationFilter
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
---
+++ src/main/java/org/samydevup/blogrestapi/payload/JWTAuthResponse.java
@@ -0,0 +1,16 @@
+package org.samydevup.blogrestapi.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JWTAuthResponse {
+
+    private String accessToken;
+    private String tokenType="Bearer";
+}
