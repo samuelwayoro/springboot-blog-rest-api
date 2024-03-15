a7016476152f1bf408c23abc57f49cc5d16e20b5
Samuel Wayoro <samuelwayoro@gmail.com>
2024-03-15 20:07:06 +0100

    exemple de versioning type 1 : versioning de l'API a travers l'url des endpoints

--- src/main/java/org/samydevup/blogrestapi/controller/PostController.java
+++ src/main/java/org/samydevup/blogrestapi/controller/PostController.java
@@ -6,18 +6,21 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.samydevup.blogrestapi.payload.PostDto;
+import org.samydevup.blogrestapi.payload.PostDtoV2;
 import org.samydevup.blogrestapi.payload.PostResponse;
 import org.samydevup.blogrestapi.service.PostService;
 import org.samydevup.blogrestapi.utils.AppConstants;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping()
 @Tag(//annotation swagger permettant customiser le swagger-ui avec des infos en plus
         name = "CRUD REST APIs for Post Resource"
 )
@@ -47,7 +50,7 @@ public class PostController {
             responseCode = "201",
             description = "Http Status 201 CREATED"
     )
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         /**
          PostDto postDto1 = postService.createPost(postDto);
@@ -58,8 +61,8 @@ public class PostController {
     }
 
     @Operation(summary = "Get all Posts REST API", description = "Get all Posts REST API is used to fetch all the posts from the database")
-    @ApiResponse(responseCode = "200",description = "Http Status 200 SUCCESS")
-    @GetMapping
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
+    @GetMapping("/api/v1/posts")
     public PostResponse getAllPosts(@RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
                                     @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
                                     @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
@@ -68,33 +71,53 @@ public class PostController {
     }
 
     @Operation(summary = "Get Post by id REST API", description = "Get Post By Id REST API is used to get single post from the database")
-    @ApiResponse(responseCode = "200",description = "Http Status 200 SUCCESS")
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable("id") Long id) {
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
+    @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable("id") Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @Operation(summary = "Get Post by id REST API", description = "Get Post with tag's informations , by id rest api is used to get single post from the database")
+    @ApiResponse(responseCode = "200", description = "Http status 200 success")
+    @GetMapping("/api/v2/posts/{id}")
+    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable("id") Long id) {
+        PostDto postDto = postService.getPostById(id);
+        System.out.println(postDto.toString());
+        PostDtoV2 postDtoV2 = new PostDtoV2();
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setContent(postDto.getContent());
+        List<String> tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("Spring Boot");
+        tags.add("AWS");
+        postDtoV2.setTags(tags);
+        System.out.println(postDtoV2.toString());
+        return ResponseEntity.ok(postDtoV2);
+    }
+
     @Operation(summary = "Update Post REST API", description = "Update Post REST API is used to update a particular post in the database")
-    @ApiResponse(responseCode = "200",description = "Http Status 200 SUCCESS")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
     @SecurityRequirement(name = "Bear Authentication")
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable("id") Long id) {
         PostDto updatedPostDtoResponse = postService.updatePost(postDto, id);
         return new ResponseEntity<>(updatedPostDtoResponse, HttpStatus.OK);
     }
 
     @Operation(summary = "Delete Post REST API", description = "Delete Post REST API is used to delete a particular post in the database")
-    @ApiResponse(responseCode = "200",description = "Http Status 200 SUCCESS")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
     @SecurityRequirement(name = "Bear Authentication")
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public ResponseEntity<String> deletePost(@PathVariable("id") Long id) {
         postService.DeletePostById(id);
         return new ResponseEntity<>("Post deleted successfuly", HttpStatus.OK);
     }
 
-    @GetMapping("/category/{id}")
+    @GetMapping("/api/v1/posts/category/{id}")
     public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable("id") Long categoryId) {
         return new ResponseEntity<>(postService.getPostsByCategory(categoryId), HttpStatus.OK);
     }
